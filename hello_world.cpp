1f126011e863cbb7a2e8509c9330dc20600650be
mixa2005 <popoff.nikitos891150@gmai.com>
2025-03-27 17:01:59 +0700

    Add code comments

--- hello_world.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-
-int main() {
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
-    std::cout << "Hello world from " << name << "!" << std::endl;
-    return 0;
-}
